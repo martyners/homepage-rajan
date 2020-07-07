fcac8c0e5daa9ed308f4ac9fdace74257c474859
Martyna Michalak-Halinska <martyna.michalak@gmail.com>
2020-07-07 12:36:55 +0200

    Added arrow function update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
-console.log(" Łapa!");
+{
+    const welcome = () => {
+        console.log(" Łapa!");
+    };
 
-let hideImageButton = document.querySelector(".js-hideImageButton");
-let profilePicture = document.querySelector(".js-profilePicture");
+    const hideImageClick = () => {
+        const profilePicture = document.querySelector(".js-profilePicture");
+        profilePicture.classList.toggle("js-profilePictureDisappear");
+        const hideImageButton = document.querySelector(".js-hideImageButton");
+        hideImageButton.innerText = profilePicture.classList.contains("js-profilePictureDisappear") ? "Pojawiam się" : "Znikam"
+    };
 
-hideImageButton.addEventListener("click", () => {
-    profilePicture.classList.toggle("js-profilePictureDisappear");
-    hideImageButton.innerText = profilePicture.classList.contains("js-profilePictureDisappear") ? "Pojawiam się" : "Znikam"
-})
-
-let yogaButton = document.querySelector(".js-yogaButton");
-let yogaPicture = document.querySelector(".js-yogaPicture")
-
-yogaButton.addEventListener("click", () => {
-    yogaPicture.classList.toggle("js-yogaTransform");
-})
-
-let whatDoingButton = document.querySelector(".js-whatDoingButton");
-let whatDoingResponse = document.querySelector(".js-whatDoingResponse");
-let date = new Date();
-let time = date.getHours();
+    const transformYogaPicture = () => {
+        const yogaPicture = document.querySelector(".js-yogaPicture")
+        yogaPicture.classList.toggle("js-yogaTransform");
+    };
 
 
-whatDoingButton.addEventListener("click", () => {
-    if (time < 8 || time > 21) {
-        whatDoingResponse.innerText = "Śpię, jest teraz " + time;
-    } else if (time > 10 && time < 13) {
-        whatDoingResponse.innerText = "Odpoczywam po śniadaniu, jest teraz " + time;
-    } else if (time > 15 && time < 19) {
-        whatDoingResponse.innerText = "Odpoczywam po obiedzie, jest teraz " + time;
-    } else {
-        whatDoingResponse.innerText = "Obchód dzielni, jest teraz " + time;
-    }
-});
+    const whatDoing = () => {
+        const whatDoingResponse = document.querySelector(".js-whatDoingResponse");
+        const date = new Date();
+        const time = date.getHours();
+        if (time < 8 || time > 21) {
+            whatDoingResponse.innerText = "Śpię, jest teraz " + time;
+        } else if (time > 10 && time < 13) {
+            whatDoingResponse.innerText = "Odpoczywam po śniadaniu, jest teraz " + time;
+        } else if (time > 15 && time < 19) {
+            whatDoingResponse.innerText = "Odpoczywam po obiedzie, jest teraz " + time;
+        } else {
+            whatDoingResponse.innerText = "Obchód dzielni, jest teraz " + time;
+        }
+    };
+
+
+    const init = () => {
+        const hideImageButton = document.querySelector(".js-hideImageButton");
+        hideImageButton.addEventListener("click", (hideImageClick));
+
+        const yogaButton = document.querySelector(".js-yogaButton");
+        yogaButton.addEventListener("click", (transformYogaPicture));
+
+
+        const whatDoingButton = document.querySelector(".js-whatDoingButton");
+        whatDoingButton.addEventListener("click", (whatDoing));
+
+        welcome();
+    };
+
+    init();
+
+
+
+
+
+
+
+};
